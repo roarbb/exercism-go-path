e8cf1b7705f4d0a90605c3c55b60b86f6c0a0b83
Matej Sajgal <roarbb@gmail.com>
2019-03-15 21:13:26 +1100

    Add hamming.go that calculate the Hamming difference between two DNA strands.

--- hamming/hamming.go
+++ hamming/hamming.go
@@ -1,4 +1,24 @@
+// Package hamming helps with counting the DNA differences
 package hamming
 
+import (
+	"errors"
+)
+
+// Distance returns hamming distance between 2 DNAs
 func Distance(a, b string) (int, error) {
+
+	if len(a) != len(b) {
+		return -1, errors.New("Error: Different length of strings")
+	}
+
+	var distance int
+
+	for index := 0; index < len(a); index++ {
+		if a[index] != b[index] {
+			distance++
+		}
+	}
+
+	return distance, nil
 }
